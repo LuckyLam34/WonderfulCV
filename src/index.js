4e187190fefb50076115dd8370da5c83a63c051c
LuckyLam34 <tutrinh3004@gmail.com>
2017-02-26 22:04:43 +0700

    init project structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/app';
+import './styles/style.scss';
+
+render(<App/>, document.getElementById('main'));
