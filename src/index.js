cd177c4f6997327209fcc74844281f537fc1ae0c
LuckyLam34 <tutrinh3004@gmail.com>
2017-02-28 23:54:06 +0700

    added dynCss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+
+/* Dyn Css */
+import '../bower_components/jquery/dist/jquery.min.js';
+import '../bower_components/dyn-css/lib/dyncss.js';
+
 import './styles/style.scss';
 
 render(<App/>, document.getElementById('main'));
